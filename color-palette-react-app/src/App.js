0b13a7524dea1446c66ff48ed6e77c4949ad8fd4
piyushshri01 <piyushshrimal94@gmail.com>
2020-08-14 23:18:06 -0700

    "Fix Palette Route"

--- color-palette-react-app/src/App.js
+++ color-palette-react-app/src/App.js
@@ -6,11 +6,23 @@ import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
 class App extends Component {
+  findPalette = (id) => {
+    return seedColors.find(function(palette){
+      return palette.id === id
+    });
+  }
   render() {
     return (
       <Switch>
         <Route exact path="/"/>
-        <Route exact path="/palette/:id"/>
+        <Route 
+        exact 
+        path='/palette/:id'
+        render={routeProps => (<Palette 
+          palette={generatePalette(this.findPalette(routeProps.match.params.id)
+          )}/>
+          )}
+        />
       </Switch>
       // <div className="App">
       //   <Palette palette={generatePalette(seedColors[4])}/>
