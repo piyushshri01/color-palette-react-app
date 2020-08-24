aabffd92c8787003f297de8733a448dc29cf0687
piyushshri01 <piyushshrimal94@gmail.com>
2020-08-24 09:53:36 -0700

    "Move styles out and add constants file"

--- color-palette-react-app/src/PaletteFormNav.js
+++ color-palette-react-app/src/PaletteFormNav.js
@@ -10,49 +10,10 @@ import MenuIcon from "@material-ui/icons/Menu";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
+import styles from './styles/PaletteFormNavStyles';
 
-const drawerWidth = 400;
-const styles = theme => ({
-    root: {
-        display: "flex",
-    },
-    appBar: {
-        transition: theme.transitions.create(["margin", "width"], {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.leavingScreen
-        }),
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        height: "64px",
-      },
-      appBarShift: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth,
-        transition: theme.transitions.create(["margin", "width"], {
-          easing: theme.transitions.easing.easeOut,
-          duration: theme.transitions.duration.enteringScreen
-        })
-      },
-      menuButton: {
-        marginLeft: 12,
-        marginRight: 20
-      },
-      hide: {
-        display: "none"
-      },
-      navBtns: {
-        marginRight: "1rem",
-        "& a":{
-          textDecoration: "none",
-        }
-      },
-      button: {
-        margin: "0 0.5rem",
-      },
-});
 
-export class PaletteFormNav extends Component {
+class PaletteFormNav extends Component {
     constructor(props){
         super(props);
         this.state = {
