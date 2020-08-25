2776d60aab13fe60d9090070ce8f9c1484a2b782
piyushshri01 <piyushshrimal94@gmail.com>
2020-08-24 21:09:08 -0700

    "Make PaletteList responsive"

--- color-palette-react-app/src/styles/PaletteListStyles.js
+++ color-palette-react-app/src/styles/PaletteListStyles.js
@@ -1,3 +1,4 @@
+import sizes from './sizes';
 export default {
     root: {
         backgroundColor: "blue",
@@ -12,6 +13,15 @@ export default {
         alignItems: "flex-start",
         flexDirection: "column",
         flexWrap: "wrap",
+        [sizes.down("xl")]: {
+            width: "75%",
+        },
+        [sizes.down("md")]: {
+            width: "70%",
+        },
+        [sizes.down("xs")]: {
+            width: "65%",
+        },
     },
     nav: {
         display: "flex",
@@ -28,6 +38,13 @@ export default {
         width: "100%",
         display: "grid",
         gridTemplateColumns: "repeat(3, 30%)",
-        gridGap: "5%",
+        gridGap: "2.5rem",
+        [sizes.down("md")]: {
+            gridTemplateColumns: "repeat(2, 50%)",
+        },
+        [sizes.down("xs")]: {
+            gridTemplateColumns: "repeat(1, 100%)",
+            gridGap: "1rem",
+        },
     }
 }
